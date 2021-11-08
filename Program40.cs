15192e18268f1d9f0e58dab32f0ea5cf8542d5a7
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-11-08 23:32:16 +0900

    [Add]C#

--- Program40.cs
+++ Program40.cs
@@ -1,21 +1,24 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
- 
-namespace Sample702
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Access a = new Access();
-            //a.Data1 = 1;
-            a.Data2 = 2;
-            a.ShowDatas();
-            Console.WriteLine("a.data1 = {0}", a.Data1);
-            //Console.WriteLine("a.data2 = {0}", a.Data2);
-        }
-    }
-}
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	namespace Sample703
+	{
+		   class Program
+		   {
+		       static void Main(string[] args)
+		       {
+		           Person2 p = new Person2();
+		           //  名前と年齢を設定
+		           p.SetAgeAndName("山田太郎", 26);
+		           //  年齢の変更
+		           p.Age = 32;
+		           //  名前の変更（できない）
+		           //  p.Name = 36;
+		           //  名前と年齢の表示
+		           Console.WriteLine("名前：{0}　年齢:{1}", p.Name, p.Age);
+		       }
+		   }
+	}
